cde3920aa757ef77291056962ad9a884be7b1c1a
Aarat P Chopra <aarat.chopra@gmail.com>
2023-02-01 07:44:27 +0530

    Added the Bound<>Operator classes

--- CodeAnalysis/Binding/BoundBinaryExpression.cs
+++ CodeAnalysis/Binding/BoundBinaryExpression.cs
@@ -1,18 +1,19 @@
 ﻿namespace APCCompiler.CodeAnalysis.Binding
 {
+
     internal sealed class BoundBinaryExpression : BoundExpression
     {
-        public BoundBinaryExpression(BoundExpression left, BoundBinaryOperatorKind operatorKind, BoundExpression right)
+        public BoundBinaryExpression(BoundExpression left, BoundBinaryOperator op, BoundExpression right)
         {
             Left = left;
-            OperatorKind = operatorKind;
+            Op = op;
             Right = right;
         }
 
         public override BoundNodeKind Kind => BoundNodeKind.BinaryExpression;
         public override Type Type => Left.Type;
         public BoundExpression Left { get; }
-        public BoundBinaryOperatorKind OperatorKind { get; }
+        public BoundBinaryOperator Op { get; }
         public BoundExpression Right { get; }
     }
 }
